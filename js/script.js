f6870ecc5a3e015f7c509fe99916baaa08b54994
telmovaz <40360576+telmovaz@users.noreply.github.com>
2019-12-05 18:56:08 +0100

    lesson13

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-if (result) {
-  document.getElementById("result").textContent = result;
+let result = 4 < 1;
+
+if (100 - 10 > 80) {
+  alert('correct');
+} else {
+  alert('not correct')
 }
+
+
+document.getElementById("result").textContent = result;
